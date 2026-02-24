d63a961811a2cc52fa74960fb034089c11a906f3
Your Name <you@example.com>
2026-02-24 09:20:47 +0530

    UC2-print-using-string.join()

--- OOPSBannerapp.java
+++ OOPSBannerapp.java
@@ -2,13 +2,16 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    ******    ***** ");
-        System.out.println("*     *  *     *   *     *  *     *");
-        System.out.println("*     *  *     *   *     *  *      ");
-        System.out.println("*     *  *     *   ******    ***** ");
-        System.out.println("*     *  *     *   *              *");
-        System.out.println("*     *  *     *   *              *");
-        System.out.println(" *****    *****    *        *****  ");
+        String banner = String.join("\n",
+            "  ***     ***    ******    *****  ",
+            " **  **   ** **   **   **  **   ** ",
+            "**    ** **   **  **   **  **       ",
+            "**    ** **   **  ******    *****   ",
+            "**    ** **   **  **              **",
+            " **  **   ** **   **        **    **",
+            "  ***       ***    **         *****  "
+        );
 
+        System.out.println(banner);
     }
 }
